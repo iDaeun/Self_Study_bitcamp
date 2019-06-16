383c92c5d0737f38c34c19505094baf93f0f3ad9
Daeun Kang <ekdms2309@naver.com>
2019-06-16 15:36:31 +0900

    연산자
    3.1 연산자와 연산식
    3.2 연산의 방향과 우선순위
    3.3 단항연산자
    3.4 이항연산
    3.5 삼항 연산자

---
+++ JAVA/chap03/src/sec04/exam01_arithmatic/CharOperationExample.java
@@ -0,0 +1,17 @@
+package sec04.exam01_arithmatic;
+
+public class CharOperationExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A' + 1;
+		System.out.println(c1);
+		char c2 = 'A';
+		//char c3 = c2 + 1;
+		int result = c2 + 1;
+		System.out.println(result);
+		
+		char c3 = (char) result;
+		System.out.println(c3);
+	}
+
+}
