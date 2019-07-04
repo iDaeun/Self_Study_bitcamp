0bed6d79e82365e9d382dc0e5a4bb5f16c081035
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2019-07-04 17:22:51 +0900

    sql practice

--- Database/select_hw.sql
+++ Database/select_hw.sql
@@ -157,4 +157,92 @@ select job as "직급", sal as "원래 급여",
     ) as "인상된 급여"
 from emp
 ;
+--------------------------------------------------------------------------------------------------------
+--23. 모든 사원의 급여 최고액, 최저액, 총액 및 평균 급여를 출력하시오. 평균에 대해서는 정수로 반올림하시오.
+select max(sal) as "급여 최고액", min(sal) as "급여 최저액", sum(sal) as "총액", round(avg(sal)) as "평균급여"
+from emp
+;
+
+--24. 각 담당 업무 유형별로 급여 최고액, 최저액, 총액 및 평균 액을 출력하시오. 평균에 대해서는 정수로 반올림 하시오.
+select job,
+max(sal) as "급여 최고액", min(sal) as "급여 최저액", sum(sal) as "총액", round(avg(sal)) as "평균급여"
+from emp
+group by job
+;
+
+--25. COUNT(*) 함수를 이용하여 담당업무가 동일한 사원 수를 출력하시오.
+select job,
+    count(*)
+from emp
+group by job;
+
+--26. 관리자 수를 나열하시오.
+select count(job)
+from emp
+where job = 'MANAGER';
+
+--27. 급여 최고액, 급여 최저액의 차액을 출력하시오.
+select max(sal) - min(sal)
+from emp
+;
+
+--28. 직급별 사원의 최저 급여를 출력하시오. 
+--관리자를 알 수 없는 사원이 있는 그룹을 제외하고, 최저 급여가 2000 미만인 그룹도 제외시키고 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
+select job,
+    min(sal)
+from emp
+where not(mgr is null)
+group by job
+having min(sal) >= 2000
+order by min(sal) desc
+;
+
+--29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 소수점 둘째 자리로 반올림 하시오.
+select deptno,
+    count(*) as "사원 수", round(avg(sal),2)  as "평균급여"
+from emp
+group by deptno
+;
+
+--30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 정수로 반올림 하시오. DECODE 사용.
+select deptno as "부서번호",
+    DECODE(deptno,
+            10 , 'ACCOUNTING',
+            20 , 'RESEARCH',
+            30 , 'SALES',
+            40 , 'OPERATION'
+    ) as "부서명" ,
+    count(*) as "사원 수",
+    DECODE(deptno,
+            10 , 'NEW YORK',
+            20 , 'DALLAS',
+            30 , 'CHICAGO',
+            40 , 'BOSTON'
+    ) as "지역명",
+    round(avg(sal)) as "평균 급여"
+from emp
+group by deptno
+;
+
+
+--31. 업무를 표시한 다음 해당 업무에 대해 부서 번호별 급여 및 부서 10, 20, 30의 급여 총액을 각각 출력하시오. 
+--별칭은 각 job, dno, 부서 10, 부서 20, 부서 30, 총액으로 지정하시오. ( hint. Decode, group by )
+
+select job, deptno as "DNO",
+    decode (deptno, 10, sal) as "부서10",
+    decode (deptno, 20, sal) as "부서20",
+    decode (deptno, 30, sal) as "부서30",
+    sal as "총액"
+from emp -- 개개인만 출력됨
+;
+
+select job, deptno as "DNO",
+    decode (deptno, 10, sum(sal)) as "부서10",
+    decode (deptno, 20, sum(sal)) as "부서20",
+    decode (deptno, 30, sum(sal)) as "부서30",
+    sum(sal) as "총액"
+from emp
+group by job, deptno -- 중복되는 job, 부서번호 묶어줌
+;
+
 
