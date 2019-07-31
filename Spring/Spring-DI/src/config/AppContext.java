5d3c7668f770e8f558173f81724b20850cdcded0
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2019-07-31 20:31:47 +0900

    .

--- Spring/Spring-DI/src/config/AppContext.java
+++ Spring/Spring-DI/src/config/AppContext.java
@@ -12,7 +12,12 @@ import member.MemberRegisterService2;
 // 컨테이너가 생성될때 AppContext 클래스도 같이 생성됨
 @Configuration
 public class AppContext {
-
+	
+	//---------------------------------------------------
+	// 만약 @Bean의 name이 둘 다 같으면 -> 마지막 Bean으로 적용됨 (오류안남)
+	// 만약 @Bean의 두 name이 다르고, type은 같으면 -> 겹치므로 오류남 => @Qualifier써서 구분해주기
+	// (AppContext클래스 & Service클래스 모두 설정해줘야함)
+	
 	@Bean(name = "memberDao1")
 	@Qualifier("sys1")
 	//@Scope("prototype") // singleton(default) => prototype
@@ -25,6 +30,8 @@ public class AppContext {
 	public BoardDao boardDao() {
 		return new BoardDao();
 	}
+	
+	//---------------------------------------------------
 	
 	@Bean(name = "registService2")
 	public MemberRegisterService2 getMemberRegisterService2() {
--- Spring/SpringMVCProject/src/main/java/com/bitcamp/mvc/HelloController.java
+++ Spring/SpringMVCProject/src/main/java/com/bitcamp/mvc/HelloController.java
@@ -6,15 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller // requestMapping 읽을 수 있도록 @Controller라고 설
+@Controller // requestMapping 읽을 수 있도록 @Controller라고 설정
 public class HelloController {
 	
+	// DefaultAnnotationHandlerMapping -> @RequestMapping어노테이션 사용
+	// 클라이언트 요청 URL과 Controller를 매핑하는 것 (=controller결정)
 	@RequestMapping("/hello")
 	public ModelAndView hello() {
 		
 		// FrontController로 전송할 View 경로 & 결과 데이터 -> 저장하고 전달할 객체
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("hello"); // /WEB-INF/views/hello.jsp
+		modelAndView.setViewName("hello"); // /WEB-INF/views/hello.jsp으로 이동하게함 (ViewResolver에 인해서)
 		modelAndView.addObject("userName", "Cool"); // request속성으로 공유
 		modelAndView.addObject("greeting","안녕하세요");
 		modelAndView.addObject("now",new Date());
@@ -22,6 +24,7 @@ public class HelloController {
 		return modelAndView;
 	}
 	
+	// 추가적으로 설정할 수도 있음
 	@RequestMapping("/hello1")
 	public ModelAndView hello1() {
 		
