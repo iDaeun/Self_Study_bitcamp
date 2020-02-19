0d1e6639fe644d1c3dd72f9963859e56d2484b6a
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2020-02-19 21:03:11 +0900

    field

---
+++ JAVA/chap06/sec06/exam02_field_default_value/FieldInitValueExample.java
@@ -0,0 +1,20 @@
+package sec06.exam02_field_default_value;
+
+public class FieldInitValueExample {
+
+	public static void main(String[] args) {
+		FieldInitValue fiv = new FieldInitValue();
+		
+		System.out.println("byteField: " + fiv.byteField);
+		System.out.println(fiv.shortField);
+		System.out.println(fiv.intField);
+		System.out.println(fiv.longField);
+		System.out.println(fiv.booleanField);
+		System.out.println(fiv.charField);
+		System.out.println(fiv.floatField);
+		System.out.println(fiv.doubleField);
+		System.out.println(fiv.arrField);
+		System.out.println(fiv.referencesField);
+	}
+
+}
