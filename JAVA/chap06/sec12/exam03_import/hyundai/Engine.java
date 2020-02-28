ab1d4ce758a7561455da3227ab637774db73e89c
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2020-02-28 12:10:14 +0900

    import

---
+++ JAVA/chap06/sec12/exam03_import/hyundai/Engine.java
@@ -0,0 +1,10 @@
+package sec12.exam03_import.hyundai;
+
+public class Engine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAVA/chap06/sec12/exam03_import/mycompany/Car.java
@@ -0,0 +1,13 @@
+package sec12.exam03_import.mycompany;
+
+import sec12.exam03_import.hankook.*;
+import sec12.exam03_import.hankook.Tire;
+import sec12.exam03_import.hyundai.*;
+import sec12.exam03_import.kumho.*;
+
+public class Car {
+ Engine engine = new Engine();
+ SnowTire tire1 = new SnowTire();
+ BigWirdthTire tire2 = new BigWirdthTire();
+ Tire tire3 = new Tire(); 
+}
