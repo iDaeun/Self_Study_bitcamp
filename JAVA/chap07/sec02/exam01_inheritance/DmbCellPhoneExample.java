8cf03f95dd48e2f3d301e3cc288feeb66c3211ad
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2020-03-05 21:12:43 +0900

    inheritance

---
+++ JAVA/chap07/sec02/exam01_inheritance/DmbCellPhoneExample.java
@@ -0,0 +1,17 @@
+package sec02.exam01_inheritance;
+
+public class DmbCellPhoneExample {
+
+	public static void main(String[] args) {
+		DmbCellPhone dmbCellPhone = new DmbCellPhone("자바폰", "검정", 10);
+		
+		System.out.println("모델: " + dmbCellPhone.model);
+		System.out.println("색상: " + dmbCellPhone.color);
+		
+		System.out.println("채널: " + dmbCellPhone.channel);
+		
+		dmbCellPhone.powerOn();
+		
+	}
+
+}
