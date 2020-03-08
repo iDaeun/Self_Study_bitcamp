f830bd5941876419842b071c24e24642406075dc
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2020-03-08 12:03:16 +0900

    protected

---
+++ JAVA/chap07/sec06/exam01_protected_package2/C.java
@@ -0,0 +1,11 @@
+package sec06.exam01_protected_package2;
+
+import sec06.exam01_protected_package1.*;
+
+public class C {
+	public void method() {
+		/*
+		 * A a = new A (); a.field = "value"; a.method();
+		 */
+	}
+}
---
+++ JAVA/chap07/sec06/exam01_protected_package2/D.java
@@ -0,0 +1,16 @@
+package sec06.exam01_protected_package2;
+
+import sec06.exam01_protected_package1.*;
+
+public class D extends A {
+	public D() {
+		super();
+		this.field = "value";
+		this.method();
+	}
+	
+	public void method() {
+		this.field = "value";
+		this.method();
+	}
+}
