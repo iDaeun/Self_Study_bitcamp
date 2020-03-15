77220a0cfd8ba159f017f8108d71972d9590c6b1
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2020-03-15 23:49:20 +0900

    interface declaration, constant field, abstract, default, static method

---
+++ JAVA/chap08/sec02/exam03_abstract_method/RemoteControl.java
@@ -0,0 +1,10 @@
+package sec02.exam03_abstract_method;
+
+public interface RemoteControl {
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;
+	
+	void turnOn();
+	void turnOff();
+	void setVolume(int volume);
+}
