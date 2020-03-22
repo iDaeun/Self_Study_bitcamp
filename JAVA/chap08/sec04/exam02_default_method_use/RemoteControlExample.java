58937f9007298c19e6c836d44cf069b5b0da8497
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2020-03-22 23:33:22 +0900

    abstract, default, static

---
+++ JAVA/chap08/sec04/exam02_default_method_use/RemoteControlExample.java
@@ -0,0 +1,19 @@
+package sec04.exam02_default_method_use;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl rc = null;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.turnOff();
+		rc.setMute(true);
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.turnOff();
+		rc.setMute(true);
+	}
+
+}
